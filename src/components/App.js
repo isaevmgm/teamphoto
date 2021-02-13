f24c3890d0cefef458eda9113321f5b88b39ebdf
Magomed <eisenbergg13@mail.ru>
2021-02-13 19:53:50 +0300

    jjj

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {loadAlbums} from "../redux/actions";
+import Albums from "./Albums";
+import Photos from "./Photos";
+import {Route} from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch();
@@ -10,9 +13,18 @@ function App() {
   }, []);
 
   return (
-    <div>
-
-    </div>
+      <Route path="/:id?">
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <Albums />
+            </div>
+            <div className="col">
+              <Photos />
+            </div>
+          </div>
+        </div>
+      </Route>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
