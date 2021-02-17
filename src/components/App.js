f52adaaee18f7f78b22fa786a81dccfb6b686ca5
Magomed <eisenbergg13@mail.ru>
2021-02-17 20:34:42 +0300

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { loadAlbums } from '../redux/actions';
-import Albums from './Albums';
-import Photos from './Photos';
-import { Route } from 'react-router-dom';
-=======
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {loadAlbums} from "../redux/actions";
 import Album from "./Album";
 import Photos from "./Photos";
 import {Route} from 'react-router-dom';
-
->>>>>>> 6d5f3d5f3a90ac3fa3576a54499136bceed7c7f9
 
 function App() {
   const dispatch = useDispatch();
@@ -23,30 +13,20 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <Route path="/:id?">
-      <div className="container">
-        <div className="row">
-          <div className="col-3">
-            <Albums />
-          </div>
-          <div className="col">
-            <Photos />
-=======
       <Route path="/:id?">
-        <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <Album />
-            </div>
-            <div className="col">
-              <Photos />
-            </div>
->>>>>>> 6d5f3d5f3a90ac3fa3576a54499136bceed7c7f9
+          <div>
+              <div className="container">
+                  <div className="row">
+                      <div className="col-3">
+                          <Album />
+                      </div>
+                      <div className="col">
+                          <Photos />
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
-    </Route>
+      </Route>
   );
 }
 
